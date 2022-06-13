69063f2211d9063a7ba5494ed4b826f0e7f74f7e
kingvyas <kumarvyasss@gmail.com>
2022-06-13 18:10:13 +0530

    fft

--- src/components/home/SingleNote.jsx
+++ src/components/home/SingleNote.jsx
@@ -1,15 +1,42 @@
-import { useEffect, useRef } from "react";
-import EditorJS from "@editorjs/editorjs"
+import { useState } from "react";
+
+import previewIcon from "../../assets/home/preview.svg";
+import editIcon from "../../assets/home/edit.svg";
+import saveIcon from "../../assets/home/save.svg";
+
+import { RichTextEditor } from "@mantine/rte";
+import editorConfig from "./editorConfig";
 
 export default function SingleNote() {
+  const [editorValue, setEditorValue] = useState("Start writing from here ...");
+  const [previewEditor, setPreviewEditor] = useState(false);
 
-  new EditorJS({holder: "editor"})
+  const handlePreview = () => {
+    setPreviewEditor((prev) => !prev);
+  };
+  
+  const handleSave = () => {
+    setPreviewEditor(true);
+    console.log(editorValue)
+  };
 
   return (
     <div className="right-side">
       <div className="upper-sec">
         <div className="title-wrapper">
           <input type="text" placeholder="Enter note title" />
+        </div>
+        <div className="button-menu-wrapper">
+          <div
+            className="button-wrapper"
+            title="Preview"
+            onClick={handlePreview}
+          >
+            <img className="preview" src={previewIcon} />
+          </div>
+          <div className="button-wrapper" title="Save" onClick={handleSave}>
+            <img className="save" src={saveIcon} />
+          </div>
         </div>
       </div>
       <div className="note-info">
@@ -28,7 +55,12 @@ export default function SingleNote() {
       </div>
       <hr />
       <div className="editor-wrapper">
-        <div id="editor"></div>
+        <RichTextEditor
+          value={editorValue}
+          controls={editorConfig}
+          onChange={setEditorValue}
+          readOnly={previewEditor}
+        />
       </div>
     </div>
   );
---
+++ src/components/home/editorConfig.js
@@ -0,0 +1,8 @@
+export default [
+  ["bold", "italic", "underline", "strike", "clean"],
+  ["h1", "h2", "h3", "h4", "h5", "h6"],
+  ["unorderedList", "orderedList"],
+  ["link", "code", "codeBlock", "blockquote"],
+  ["alignLeft", "alignCenter", "alignRight"],
+  ["sub", "sup"],
+];
